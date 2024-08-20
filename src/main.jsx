5f1d684dee9a91d8e365d49c2b62a96e613ac355
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-08-20 14:42:25 +0530

    Feat : added analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
