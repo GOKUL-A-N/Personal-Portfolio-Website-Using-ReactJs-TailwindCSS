8cb323664e7ea87a420e9f3ec7fc7d412ba666fb
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-08-20 14:36:00 +0530

    Fix : reconfigured text size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,14 +21,14 @@ export default function Home() {
       >
           <div className='w-full h-full flex flex-col items-center justify-center'>
         <h6 onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}} className='text-[10px] sm:text-[12px] font-semibold text-center tracking-widest uppercase'>Gokul NatarajMoorthy</h6> 
-        <h1 onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}} className='text-center text-[35px] sm:text-[35px] leading-tight md:text-[65px] xl:text-[65px]   uppercase font-black'><span>Robbing</span> <br /> <span>Good</span> <br /> <span>Designs</span> <br /> <span>Since</span> <br /> <span>2021</span> </h1>
+        <h1 onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}} className='text-center text-[40px] sm:text-[45px] leading-tight md:text-[75px] xl:text-[85px]   uppercase font-black'><span>Robbing</span> <br /> <span>Good</span> <br /> <span>Designs</span> <br /> <span>Since</span> <br /> <span>2021</span> </h1>
         </div> 
       </motion.div>
 
       <div className={`${styles.body} h-full w-full`}>
         <div className='w-full h-full flex flex-col items-center justify-center '>
         <h6 className='reveal delay-700 text-[10px] sm:text-[12px] font-semibold text-center tracking-widest uppercase'>Gokul NatarajMoorthy</h6> 
-        <h1 className='reveal delay-100 text-center text-[35px] sm:text-[35px] leading-tight md:text-[65px] xl:text-[65px]  md:tracking-normal  uppercase font-black'><span>Making</span><br /> <span className='text-[#ff512e]'>Good</span> <br /> <span className='text-[#ff512e]'>Designs</span> <br /> <span>Since</span> <br /> <span>2021</span> </h1>
+        <h1 className='reveal delay-100 text-center text-[40px] sm:text-[45px] leading-tight md:text-[75px] xl:text-[85px]  md:tracking-normal  uppercase font-black'><span>Making</span><br /> <span className='text-[#ff512e]'>Good</span> <br /> <span className='text-[#ff512e]'>Designs</span> <br /> <span>Since</span> <br /> <span>2021</span> </h1>
         </div> 
       </div>
 
