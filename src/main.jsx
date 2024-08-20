46ce4f42940c6e1ba14e7f7afb383063808d3384
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-08-20 14:47:12 +0530

    Fix : renamed file name

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
