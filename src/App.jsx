7096606ed682f856ea69ed6e89b2a98a2f6ff097
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-08-20 16:23:32 +0530

    Fix : Fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Home from './components/Home'
 import About from './components/About'
 import {Routes , Route} from "react-router-dom"
